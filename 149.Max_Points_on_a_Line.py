be1f84573e03489174420ff945b8e3ae94dd3ed0
Zhixuan Wang <Zhixuan.Wang@experian.com>
2015-08-24 22:35:48 -0700

    149 passed the online judger

--- 149.Max_Points_on_a_Line.py
+++ 149.Max_Points_on_a_Line.py
@@ -12,40 +12,42 @@ class Solution(object):
             return 1
         elif len(points)==2:
             return 2
-        
-        
+
         lines = {}
         distinct_points = {}
         for point in points:
             distinct_points[(point.x, point.y)] = distinct_points.get((point.x, point.y),0) + 1
-                
+
         if len(distinct_points)==1:
             return len(points)
-            
-            
-        for i,point in enumerate(distinct_points.keys()):
+
+        for i, point in enumerate(distinct_points.keys()):
             for j in range(i+1, len(distinct_points)):
                 point2 = distinct_points.keys()[j]
-                if (point[0] == point2[0]):
+                if point[0] == point2[0]:
                     k = 'inf'
                     b = point[0]
                 else:
-                    k = (point2[1] - point[1])/(point2[0] - point[0])
+                    k = (point2[1] - point[1])/(point2[0] - point[0]+0.0)
                     b = point[1] - k * point[0]
+#                print point, point2
+#                print k, b
+#                print '-----------'
                 if (k,b) in lines:
                     for p in (point, point2):
                         if p not in lines[(k,b)]['points']:
                             lines[(k,b)]['points'].add(p)
                             lines[(k,b)]['counts'] += distinct_points[p]
                 else:
-                    lines[(k,b)] = {'points':set([point, point2]), 
-                                    'counts':distinct_points[point] + distinct_points[point2]}
-        
+                    lines[(k, b)] = {'points': set([point, point2]),
+                                     'counts': distinct_points[point] + distinct_points[point2]}
+
+#        for key in lines:
+#            print key, lines[key]
         max_count = 2
         for key in lines:
             max_count = max(lines[key]['counts'], max_count)
-            
-            
+
         return max_count
 
 s = Solution()
@@ -55,7 +57,6 @@ s = Solution()
 
 test = [[84,250],[0,0],[1,0],[0,-70],[0,-70],[1,-1],[21,10],[42,90],[-42,-230]]
 # should give 6
-
 
 print s.maxPoints(listOfPoints(test))
 
--- datastructure/point.py
+++ datastructure/point.py
@@ -1,12 +1,16 @@
 class Point:
     def __init__(self,x=0,y=0):
-        self.x=0
-	self.y=0
+        self.x=x
+        self.y=y
+
+    def __str__(self):
+        return '('+str(self.x)+', '+str(self.y)+')'
 
 def listOfPoints(input_list):
     result = []
     for point in input_list:
         result.append(Point(point[0],point[1]))
+
     return result
 
 
